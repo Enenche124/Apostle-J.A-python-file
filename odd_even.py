9e5dfe385c212bb82221b848b0a6ef7cac5dbb7c
JOHN ADAH <enenchejohn56@gmail.com>
2024-12-04 10:11:37 +0100

    add odd_even.py

---
+++ odd_even.py
@@ -0,0 +1,6 @@
+user_value = int(input("Enter an integer number: "))
+
+if user_value % 2 == 0 : 
+   print("The number is an even number ", user_value)
+if user_value % 2 != 0 :
+     print("The number is an odd number ", user_value)
