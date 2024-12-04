fe913d293471178606a748a6799a7491b307b402
JOHN ADAH <enenchejohn56@gmail.com>
2024-12-04 20:07:12 +0100

    add odd_even.py

---
+++ largest_smallest.py
@@ -0,0 +1,31 @@
+
+
+
+
+largest_number = int(input("Enter first integer: " ))
+
+second_number = int(input("Enter first integer: " ))
+
+third_number = int(input("Enter first integer: " ))
+
+sum = largest_number + second_number + third_number
+
+average =  sum / 3
+
+
+if  second_number > largest_number:
+ 
+ largest_number = second_number
+
+if third_number >  largest_number:
+  largest_number = third_number
+
+
+
+
+print("The sum of added number is ", sum,  " The average is ", average, " and the largest is ", largest_number)
+
+
+
+
+
